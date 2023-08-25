91a7ea449bbbfaafac028dee07095d7145ad0c7a
iwang1129 <tatekuni469@icloud.com>
2023-08-25 17:07:00 +0900

    migrate 変更

--- db/migrate/20230818121025_create_comments.rb
+++ db/migrate/20230818121025_create_comments.rb
@@ -2,8 +2,6 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :content, null: false
-      t.integer :user_id
-      t.integer :prototype_id
       t.references :prototype, null: false, foreign_key: true 
       t.references :user, null: false, foreign_key: true 
       t.timestamps
