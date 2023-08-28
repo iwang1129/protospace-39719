86e3d40f04373f5523cb57bbc3aa4e7bb03356ac
iwang1129 <tatekuni469@icloud.com>
2023-08-28 11:38:48 +0900

    修正３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,16 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:edit, :show]
-
+  before_action :set_prototype, except: [:index, :new, :create]
+  # ログインしていなくても、詳細ページに遷移できる仕様にするためにexcept: [:index, :show, :create]としている
+  before_action :authenticate_user!, except: [:index, :show]
+  # ログアウト状態のユーザーであっても、トップページ・プロトタイプ詳細ページ・ユーザー詳細ページ・ユーザー新規登録ページ・ログインページには遷移できること
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
   def index
 
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
+     # includesメソッドを使用することで、prototypesのすべてのデータを取得し、
+      # prototypesテーブルに紐づくusersテーブルのデータを取得できる
+      # includesメソッドを使用するとすべてのレコードを取得するため、allメソッドは省略可能
   end
 
   def new
@@ -27,19 +34,22 @@ class PrototypesController < ApplicationController
     end
   
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-    redirect_to root_path
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+    # 空の入力欄がある場合は、編集できずにそのページに留まること
+    # 正しく編集できた場合は、詳細ページへ遷移すること
   end
 
 
 
     def show
-      @prototype = Prototype.find(params[:id])
       @comment = Comment.new
       @comments = @prototype.comments.includes(:user)
     end
@@ -59,6 +69,12 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
   
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
+   # 特定のアクション内で「投稿者確認 (contributor_confirmation)」を行うためのメソッド
+  # このメソッドが呼び出されると、ログインしているユーザーが特定のプロトタイプの投稿者であるかを確認し、
+  # 投稿者でない場合はトップページにリダイレクトする動作を行う。
 end
 
 
